a681d43a75af673846a193d9e78528786663a869
affanali397 <54842037+affanali397@users.noreply.github.com>
2020-09-11 12:48:33 +0530

    Completed

---
+++ data.py
@@ -0,0 +1,16 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title': 'Article one',
+            'author': 'Affan',
+            'create_date':'10-09-2020'
+        },
+        {
+            'id':2,
+            'title': 'Article Two',
+            'author': 'India Today',
+            'create_date':'10-09-2020'
+        },
+        
+    ]
